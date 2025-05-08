77d72b48b0509238f866ba803a6bb118e118ef3a
Jaekwon Han <indegser@gmail.com>
2025-05-09 01:47:06 +0900

    feat: Add root selector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-import { getAllChords } from "@/lib/chord";
+'use client'
+
+import { getAllChords, NOTES_SHARP } from "@/lib/chord";
+import { useState } from "react";
 
 export default function Home() {
-  const rootNote = "A";
+  const [rootNote, setRootNote] = useState("C"); // Default root note
   const chords = getAllChords(rootNote);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold text-center">Chord Types</h1>
-        <div className="grid grid-cols-4 gap-8">
+        <div className="flex gap-4 items-center">
+          <h1 className="text-3xl font-bold text-center">Chord Types</h1>
+          <select className="text-center text-2xl font-bold" onChange={(e) => setRootNote(e.target.value)} value={rootNote}>
+            {NOTES_SHARP.map((note) => (
+              <option key={note} value={note}>
+                {note}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="grid grid-cols-[minmax(300px,_1fr)] gap-8">
           {chords.map((chord) => (
             <div
               key={chord.id}
