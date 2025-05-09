e9457f3652284afa27e179e06aa6cd5e47d32e4b
Jaekwon Han <indegser@gmail.com>
2025-05-09 18:59:48 +0900

    feat: Add eslint and prettier settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { getAllChords, NOTES_SHARP } from "@/lib/chord";
 import { useState } from "react";
@@ -12,7 +12,11 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center">
           <h1 className="text-3xl font-bold text-center">Chord Types</h1>
-          <select className="text-center text-2xl font-bold" onChange={(e) => setRootNote(e.target.value)} value={rootNote}>
+          <select
+            className="text-center text-2xl font-bold"
+            onChange={(e) => setRootNote(e.target.value)}
+            value={rootNote}
+          >
             {NOTES_SHARP.map((note) => (
               <option key={note} value={note}>
                 {note}
@@ -30,13 +34,13 @@ export default function Home() {
               <div className="flex gap-4">
                 {chord.notes.map((note, index) => (
                   <div key={note} className="flex flex-col gap-2 items-center">
-                  <div
-                    className="flex items-center justify-center w-8 h-8 text-gray-300 font-semibold text-xs bg-gray-700 rounded-full"
-                  >
-                    {note}
-                  </div>
-                    <div className="text-xs text-gray-400">{chord.formula[index]}</div>
+                    <div className="flex items-center justify-center w-8 h-8 text-gray-300 font-semibold text-xs bg-gray-700 rounded-full">
+                      {note}
                     </div>
+                    <div className="text-xs text-gray-400">
+                      {chord.formula[index]}
+                    </div>
+                  </div>
                 ))}
               </div>
             </div>
