03c25022f65116f4313f57b868a6700d7fe67e3a
Jaekwon Han <indegser@gmail.com>
2025-05-12 00:33:55 +0900

    fix type error

--- src/app/fretboard/finger.tsx
+++ src/app/fretboard/finger.tsx
@@ -2,14 +2,13 @@ import { FretboardPosition } from "@/lib/guitar";
 import { Note } from "../note/note";
 
 export const Finger = ({ string, fret, note, interval }: FretboardPosition) => {
-  const isRoot = interval === "1P";
   return (
     <div
       style={{ gridRow: string, gridColumn: fret + 1 }}
       // py for preventing note from being too close on another string.
       className="flex w-full justify-center py-0.5 font-[family-name:var(--font-geist-mono)]"
     >
-      <Note note={note} interval={interval} displayNote={isRoot} />
+      <Note note={note} interval={interval} />
     </div>
   );
 };
